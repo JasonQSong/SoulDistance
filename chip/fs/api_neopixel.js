cc0e223be5a91e74978a63d754fd373f5fc14d2e
Jason Song <jasonqsong@outlook.com>
2017-01-30 01:17:52 -0800

    move to js

--- chip/fs/api_neopixel.js
+++ chip/fs/api_neopixel.js
@@ -1,11 +1,13 @@
 
 let NeoPixel = {
-  __init___n_p_t: ffi('void Adafruit_NeoPixel____init___n_p_t(Adafruit_NeoPixel *this,uint16_t n,uint8_t p,neoPixelType t)'),
-  begin: ffi('void Adafruit_NeoPixel__begin(Adafruit_NeoPixel *this)'),
+  Adafruit_NeoPixel____init___n_p_t: ffi('Adafruit_NeoPixel * Adafruit_NeoPixel____init___n_p_t(uint16_t,uint8_t,uint8_t)'),
+  Adafruit_NeoPixel__begin: ffi('Adafruit_NeoPixel * Adafruit_NeoPixel__begin(Adafruit_NeoPixel *)'),
+  Adafruit_NeoPixel__setPixelColor_n_r_g_b: ffi('void Adafruit_NeoPixel__setPixelColor_n_r_g_b(Adafruit_NeoPixel *, uint16_t, uint8_t, uint8_t, uint8_t'),
+  Adafruit_NeoPixel__show: ffi('void Adafruit_NeoPixel__show(Adafruit_NeoPixel *)'),
   NEO_RGB: ((0 << 6) | (0 << 4) | (1 << 2) | (2)),
   NEO_RBG: ((0 << 6) | (0 << 4) | (2 << 2) | (1)),
   NEO_GRB: ((1 << 6) | (1 << 4) | (0 << 2) | (2)),
   NEO_GBR: ((2 << 6) | (2 << 4) | (0 << 2) | (1)),
   NEO_BRG: ((1 << 6) | (1 << 4) | (2 << 2) | (0)),
   NEO_BGR: ((2 << 6) | (2 << 4) | (1 << 2) | (0)),
-}
+};
